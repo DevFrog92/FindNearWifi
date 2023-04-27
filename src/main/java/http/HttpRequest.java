9941b78a7af973408b1d6229521fccf8b563ae1f
DevFrog92 <yongsu1481@gmail.com>
2023-04-27 23:53:09 +0900

    ✨ Implement HttpRequest get method that return api info data to json

--- src/main/java/http/HttpRequest.java
+++ src/main/java/http/HttpRequest.java
@@ -1,22 +1,31 @@
 package http;
 
-import java.io.IOException;
+import api.ApiStructure;
+import api.WIFIInfoDetail;
+import com.google.gson.Gson;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+import java.util.Arrays;
+
 public class HttpRequest {
     public HttpRequest() {}
 
-    public void get(String requestURL) {
+    public WIFIInfoDetail[] get(String requestURL) {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
                 .url(requestURL)
                 .build();
-//
         try {
-            Response response = client.newCall(request).execute();
+            ResponseBody response = client.newCall(request).execute().body();
+            Gson gson = new Gson();
+            ApiStructure apiStructure = gson.fromJson(response.string(), ApiStructure.class);
+            return apiStructure.getRow();
         } catch(Exception e) {
             System.err.println(e.toString());
         }
+
+        return null;
     }
 }
